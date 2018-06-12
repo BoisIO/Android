62faf6217bafec7299fbf018c3d2e88b8c5fada8
okkeh96 <okke_trommelen@live.nl>
2018-06-12 13:11:58 +0200

    Added file selecter & file>key conversion

--- app/src/main/java/com/gostreamyourself/android/LoginActivity.java
+++ app/src/main/java/com/gostreamyourself/android/LoginActivity.java
@@ -1,13 +1,96 @@
 package com.gostreamyourself.android;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.security.KeyFactory;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.login_loginButton)
+    Button loginBtn;
+
+    private int PICKFILE_RESULT_CODE = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+                chooseFile.setType("*/*");
+                chooseFile = Intent.createChooser(chooseFile, "Choose a file");
+                startActivityForResult(chooseFile, PICKFILE_RESULT_CODE);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==123 && resultCode==RESULT_OK) {
+            Uri selectedfile = data.getData(); //The uri with the location of the file
+
+            File pKey = new File(selectedfile.getPath());
+
+            try {
+                PrivateKey privateKey = getPemPrivateKey(pKey, "SHA256withRSA/PSS");
+                Log.i("Succesvol", "onActivityResult: " + privateKey.toString());
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+
+
+            Log.i("Testirino", "onActivityResult: " + selectedfile);
+        }
+    }
+
+    public PrivateKey getPemPrivateKey(File f, String algorithm) throws Exception {
+        FileInputStream fis = new FileInputStream(f);
+        DataInputStream dis = new DataInputStream(fis);
+        byte[] keyBytes = new byte[(int) f.length()];
+        dis.readFully(keyBytes);
+        dis.close();
+
+        String temp = new String(keyBytes);
+        String privKeyPEM = temp.replace("-----BEGIN PRIVATE KEY-----\n", "");
+        privKeyPEM = privKeyPEM.replace("-----END PRIVATE KEY-----", "");
+        //System.out.println("Private key\n"+privKeyPEM);
+
+
+        byte [] decoded = Base64.getDecoder().decode(privKeyPEM);
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+        KeyFactory kf = KeyFactory.getInstance(algorithm);
+        return kf.generatePrivate(spec);
+    }
+
+
+
 }
